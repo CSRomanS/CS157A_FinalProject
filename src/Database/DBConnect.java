fef774a293655f58e5138dca5c590afb2732e456
CSRomanS <roman.shpilberg@sjsu.edu>
2023-07-25 03:01:06 -0700

    Added connection implementation

--- src/Database/DBConnect.java
+++ src/Database/DBConnect.java
@@ -1,7 +1,32 @@
 package Database;
+import java.sql.*;
 
 public class DBConnect {
-    public static void Connect(){
+    // connection URL
+    final static String host = "localhost";
+    final static String port = "3306";
+    final static String dbName = "test";
+    final static String mysqlURL = "jdbc:mysql://"+host+":"+port+"/"+dbName;
 
+    // connection login
+    final static String username = "jdbcUser";
+    final static String password = "cs157a";
+    public static Connection Connect() {
+        // ensures the class is loaded
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException cnfe) {
+            System.out.println("Error loading driver: " + cnfe);
+        }
+
+        // Attempt connection
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(mysqlURL, username, password);
+        } catch (SQLException e){
+            System.out.println("Error connecting to database");
+            e.printStackTrace();
+        }
+        return connection;
     }
 }
