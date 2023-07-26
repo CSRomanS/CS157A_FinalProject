618e4d83f8aba630e4a242a5679f8046bb73e469
CSRomanS <roman.shpilberg@sjsu.edu>
2023-07-25 23:41:32 -0700

    Added database creation method

--- src/Database/DBConnect.java
+++ src/Database/DBConnect.java
@@ -1,16 +1,24 @@
 package Database;
 import java.sql.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
 
 public class DBConnect {
     // connection URL
     final static String host = "localhost";
     final static String port = "3306";
-    final static String dbName = "test";
+    final static String dbName = "AEB";
     final static String mysqlURL = "jdbc:mysql://"+host+":"+port+"/"+dbName;
 
     // connection login
     final static String username = "jdbcUser";
     final static String password = "cs157a";
+
+    /**
+     * Returns a connection to the database. (Must be closed manually)
+     * @return a connection to the database.
+     */
     public static Connection Connect() {
         // ensures the class is loaded
         try {
@@ -28,5 +36,49 @@ public class DBConnect {
             e.printStackTrace();
         }
         return connection;
+    }
+
+    /**
+     * Creates the database for the first time
+     */
+    public static void CreateDatabase(){
+        // ensures the class is loaded
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException cnfe) {
+            System.out.println("Error loading driver: " + cnfe);
+        }
+        // Attempt connection and run script
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://"+host+":"+port, username, password);
+            Statement statement = connection.createStatement();
+
+            // Runs each statement individually seperated by ';'
+            String script = loadSQL("DatabaseCreation.sql");
+            if(script != null) {
+                for (String i : script.split(";")){
+                    statement.executeUpdate(i);
+                }
+            }
+        } catch (SQLException e){
+            System.out.println("Error connecting to database");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Loads an SQL statement from the SQL folder in the working directory
+     * @param sqlFile Name of the file to load
+     * @return String containing the SQL file contents
+     */
+    public static String loadSQL(String sqlFile){
+        // get path
+        Path filePath = Paths.get(System.getProperty("user.dir"),"SQL",sqlFile);
+        try {
+            return Files.readString(filePath);
+        } catch(java.io.IOException e){
+            System.out.println(e.getMessage());
+            return null; // file not found
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import Database.DBConnect;
 public class Main {
     public static void main(String[] args) {
-        return;
+        DBConnect.CreateDatabase();
     }
 }
