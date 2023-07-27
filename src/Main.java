1c986c666415a112b67c8b7599f8e3070c25379d
CSRomanS <roman.shpilberg@sjsu.edu>
2023-07-27 12:35:01 -0700

    Added user account creation

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 import Database.DBConnect;
+import java.sql.Connection;
 public class Main {
     public static void main(String[] args) {
-        DBConnect.CreateDatabase();
+        Connection con = DBConnect.Connect();
+        try {
+            DBConnect.createAccount(con);
+            con.close();
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
     }
 }
