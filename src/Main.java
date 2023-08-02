d9547217d46c8832ae0998587158c475ccb155f3
CSRomanS <roman.shpilberg@sjsu.edu>
2023-08-01 18:41:07 -0700

    Cleaned up DB access/connect

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 import Database.DBConnect;
+import Database.DBAccess;
 import java.sql.Connection;
 public class Main {
     public static void main(String[] args) {
-        Connection con = DBConnect.Connect();
         try {
-            //DBConnect.createAccount(con, "userTest", "passTest", "fTest", "lTest", "164 Test St.", "cTest", "sTest", 94000);
-            //DBConnect.createReview(con, 4, "Review Text Test", "NULL", 1, 1);
-            //DBConnect.createHelpful(con, 1, 3, true);
-            //DBConnect.addToCart(con, 1, 4, 5);
-            DBConnect.placeOrder(con, 1);
-            con.close();
+            DBAccess.createAccount("userTest", "passTest", "fTest", "lTest", "164 Test St.", "cTest", "sTest", 94000);
+            DBAccess.createReview(4, "Review Text Test", "NULL", 1, 1);
+            DBAccess.createHelpful(1, 1, true);
+            DBAccess.addToCart(1, 4, 5);
+            DBAccess.placeOrder(1);
         } catch (Exception e){
             System.out.println(e.getMessage());
         }
